82cf5747158fae9f61ce3c7a9b65833a5eed1854
Ines <inesagbozo@gmail.com>
2024-02-08 20:47:02 +0100

    modification

---
+++ src/index.js
@@ -0,0 +1,66 @@
+
+    // barre de navigation responsive
+    const hamburger = document.querySelector(".hamburger")
+    const menu = document.querySelector(".menu")
+    const cross = document.querySelector(".cross")
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('mobile-menu')
+    })
+
+    cross.addEventListener('click', () => {
+        menu.classList.remove('mobile-menu')
+
+    })
+
+
+   
+    //  carousel de la section services
+    const buttons = document.querySelectorAll(".button");
+    const cards = document.querySelectorAll(".prod");
+
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            const calcnextslide = e.target.id === "next" ? 1 : -1;
+            const slideActive = document.querySelector(".active");
+            newIndex = calcnextslide + [...cards].indexOf(slideActive);
+            
+            if (newIndex < 0 ) newIndex =[...cards].length - 1;
+        if (newIndex >= [...cards].length) newIndex = 0;
+            cards[newIndex].classList.add("active");
+            slideActive.classList.remove("active");
+
+        });
+    });
+
+
+    // caroussel des best seller
+    const buttonsb = document.querySelectorAll(".button-best");
+    const bests = document.querySelectorAll(".best");
+  
+    buttonsb.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            const calcnextslidebest = e.target.id === "next-best" ? 1 : -1;
+            const slideActivebest = document.querySelector(".active-best");
+            nextIndexbests = calcnextslidebest + [...bests].indexOf(slideActivebest);
+            console.log(nextIndexbests)
+        if (nextIndexbests < 0 ) nextIndexbests =[...bests].length - 1;
+        if (nextIndexbests >= [...bests].length) nextIndexbests = 0;
+            bests[nextIndexbests].classList.add("active-best");
+            slideActivebest.classList.remove("active-best");
+       
+        });
+    });
+
+    //reaction coeur
+    const icon = document.getElementById("heart_icon");
+    
+        icon.addEventListener('click', function () {
+            // icon.classList.remove("fa-regular");
+            // icon.classList.add("fa-solid");
+            console.log("first click!");
+        })
+   
+
+    
+
